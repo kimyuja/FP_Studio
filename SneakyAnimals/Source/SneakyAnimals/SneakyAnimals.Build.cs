59464d1beae601f17a2bf1bc7b5632dde9f6db51
KSW1999 <kso8242@naver.com>
2024-06-19 15:12:21 +0900

    [Add] 테스트용 플레이어 + 아웃라인 메테리얼

--- SneakyAnimals/Source/SneakyAnimals/SneakyAnimals.Build.cs
+++ SneakyAnimals/Source/SneakyAnimals/SneakyAnimals.Build.cs
@@ -8,6 +8,6 @@ public class SneakyAnimals : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam" });
 	}
 }
