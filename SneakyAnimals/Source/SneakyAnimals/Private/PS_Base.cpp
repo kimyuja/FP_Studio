d2ca2426d7f2bbb6a0609fd9e0b5d7d318a5524e
kimyuja <kiy102205@sookmyung.ac.kr>
2024-07-29 17:47:45 +0900

    [테스트] 서버랑 클라 나눠서 코드 실행.. 의미 있나,,,

--- SneakyAnimals/Source/SneakyAnimals/Private/PS_Base.cpp
+++ SneakyAnimals/Source/SneakyAnimals/Private/PS_Base.cpp
@@ -207,10 +207,51 @@ void APS_Base::Load_Player_Appearance()
 void APS_Base::Load_Player_UserProfile()
 {
 	// 세이브 게임 0번 인덱스에서 유저 프로필을 불러옴
-	FUserProfileResult result = UFL_General::Get_UserProfile();
-	if (result.success)
+	if (UKismetSystemLibrary::IsServer(GetWorld()))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Load Player User Profile : %s"), *result.S_UserProfile.Username.ToString());
+		// 서버라면...
+		FUserProfileResult result = UFL_General::Get_UserProfile();
+		if (result.success)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Server Load Player User Profile : %s"), *result.S_UserProfile.Username.ToString());
+			//ServerRPC_Update_Player_UserProfile_Implementation(result.S_UserProfile);
+			Player_UserProfile = result.S_UserProfile;
+			OnRep_Player_UserProfile();
+			return;
+		}
+		else
+		{
+			FStructure_UserProfile tmp;
+			tmp.Username = FText::FromString(TEXT("Username"));
+			tmp.User_Avatar = T_ProfilePicture;
+			//ServerRPC_Update_Player_UserProfile_Implementation(tmp);
+			Player_UserProfile = tmp;
+			OnRep_Player_UserProfile();
+			return;
+		}
+	}
+	else
+	{
+		// 클라이언트라면...
+		FUserProfileResult result = UFL_General::Get_UserProfile();
+		if (result.success)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Client Load Player User Profile : %s"), *result.S_UserProfile.Username.ToString());
+			ServerRPC_Update_Player_UserProfile_Implementation(result.S_UserProfile);
+			return;
+		}
+		else
+		{
+			FStructure_UserProfile tmp;
+			tmp.Username = FText::FromString(TEXT("Username"));
+			tmp.User_Avatar = T_ProfilePicture;
+			ServerRPC_Update_Player_UserProfile_Implementation(tmp);
+			return;
+		}
+	}
+	//if (result.success)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("Load Player User Profile : %s"), *result.S_UserProfile.Username.ToString());
 		//if (!HasAuthority() && (result.S_UserProfile.Username.ToString() == "" || result.S_UserProfile.Username.ToString() == Cast<AGS_Lobby>(GetWorld()->GetGameState())->ServerName.ToString()))
 		//{
 		//	//  클라이언트인데 아직 유저 프로필 제대로 못 불러왔으면 다시 불러 와
@@ -229,17 +270,17 @@ void APS_Base::Load_Player_UserProfile()
 		//		}, 0.3f, false);
 		//	return;
 		//}
-		ServerRPC_Update_Player_UserProfile_Implementation(result.S_UserProfile);
-		return;
-	}
-	else
-	{
-		FStructure_UserProfile tmp;
-		tmp.Username = FText::FromString(TEXT("Username"));
-		tmp.User_Avatar = T_ProfilePicture;
-		ServerRPC_Update_Player_UserProfile_Implementation(tmp);
-		return;
-	}
+	//	ServerRPC_Update_Player_UserProfile_Implementation(result.S_UserProfile);
+	//	return;
+	//}
+	//else
+	//{
+	//	FStructure_UserProfile tmp;
+	//	tmp.Username = FText::FromString(TEXT("Username"));
+	//	tmp.User_Avatar = T_ProfilePicture;
+	//	ServerRPC_Update_Player_UserProfile_Implementation(tmp);
+	//	return;
+	//}
 }
 
 void APS_Base::Load_Player_ConnectionInfo(bool ClientReadyStatus)
